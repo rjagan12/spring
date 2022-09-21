10cd049ef73a5c57ad6c6a24a461ce3eb80edd71
rjagan12 <112706276+rjagan12@users.noreply.github.com>
2022-09-22 01:29:51 +0530

    Add files via upload

---
+++ demoservlet/src/main/java/com/i2i/exception/NullListException.java
@@ -0,0 +1,21 @@
+package com.i2i.exception;
+
+import java.lang.Exception;
+
+/**
+ * <h> NullListException </h> 
+ * <p>
+ *  Class used to create the Custom Exception      
+ * </p>
+ *
+ * @version 1.0
+ * @author Jaganathan R  
+ */
+public class NullListException extends Exception {
+
+    public NullListException(String s) {
+      
+        super(s);
+    } 
+
+}
